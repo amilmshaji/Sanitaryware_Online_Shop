1a6a41e9c70e9bc6422d49329d338ec4d3597043
amilmshaji <msamilmanadiyil2560@gmail.com>
2022-10-20 21:57:36 +0530

    for hacktoberfest

--- shop_app/views.py
+++ shop_app/views.py
@@ -117,29 +117,28 @@ def search(request):
     return render(request, 'shop.html', context)
 
 
-# def submit_review(request, product_id):
-#     url = request.META.get('HTTP_REFERER')
-#     if request.method == 'POST':
-#         try:
-#
-#             reviews = ReviewRating.objects.get(
-#                 user__id=request.user.id, product__id=product_id)
-#             form = ReviewForm(request.POST, instance=reviews)
-#             form.save()
-#             messages.success(request, 'Thank you ! Your Review is Updated')
-#             return redirect(url)
-#         except ReviewRating.DoesNotExist:
-#             form = ReviewForm(request.POST)
-#             if form.is_valid():
-#                 data = ReviewRating()
-#                 data.subject = form.cleaned_data['subject']
-#                 data.rating = form.cleaned_data['rating']
-#                 data.review = form.cleaned_data['review']
-#                 data.ip = request.META.get('REMOTE_ADDR')
-#                 data.product_id = product_id
-#                 data.user_id = request.user.id
-#                 data.save()
-#                 messages.success(
-#                     request, 'Thank You ! Your Review Has Been Submitted')
-#                 return redirect(url)
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
 
+            reviews = ReviewRating.objects.get(
+                user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you ! Your Review is Updated')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(
+                    request, 'Thank You ! Your Review Has Been Submitted')
+                return redirect(url)
