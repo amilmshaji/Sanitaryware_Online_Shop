64474043de7b9c9f97010b11d350531526535853
amilmshaji <msamilmanadiyil2560@gmail.com>
2022-10-24 09:30:29 +0530

    user profile model

--- accounts/models.py
+++ accounts/models.py
@@ -59,7 +59,7 @@ class Account(AbstractBaseUser,PermissionsMixin):
     is_admin = models.BooleanField(default=False)
 
     is_active = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=Flse)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['fname', 'lname',  'phone_number',]
@@ -75,4 +75,20 @@ class Account(AbstractBaseUser,PermissionsMixin):
 
     def has_module_perms(self, add_label):
         return True
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE)
+    address_line_1 = models.CharField(blank=True, max_length=100)
+    address_line_2 = models.CharField(blank=True, max_length=100)
+    profile_picture = models.ImageField(blank=True, upload_to='userprofile')
+    city = models.CharField(blank=True, max_length=20)
+    state = models.CharField(blank=True, max_length=20)
+    country = models.CharField(blank=True, max_length=20)
+
+    def __str__(self):
+        return self.user.first_name
+
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
 
