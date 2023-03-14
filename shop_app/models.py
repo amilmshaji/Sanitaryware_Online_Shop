1a0c95f036aa40b57257b6d6ed16cb1a8a3b8f06
amilmshaji <msamilmanadiyil2560@gmail.com>
2023-03-14 11:21:54 +0530

    chatbot

--- shop_app/models.py
+++ shop_app/models.py
@@ -26,8 +26,7 @@ class Category(models.Model):
     def get_url(self):
         return reverse('products_by_category', args=[self.slug])
 
-    def __str__(self):
-        return self.category_name  #shows the name
+       #shows the name
 
 
 
