d4640270937b228e7f95e02de3666d6e3dee33e3
amilmshaji <msamilmanadiyil2560@gmail.com>
2023-03-26 22:39:15 +0530

    admin stock limit warning

--- orders/admin.py
+++ orders/admin.py
@@ -13,21 +13,22 @@ class OrderProductAdmin(admin.ModelAdmin):
         'payment',
         'status',
         'amount',
-        'is_ordered',
         'ordered_date',
     )
+    list_editable = ['status']
 
 
-    #
-    # # This will help you to disbale add functionality
-    # def has_add_permission(self, request):
-    #     return False
-    #
-    #     # This will help you to disable delete functionaliyt
-    #
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
-    #
+
+
+    # This will help you to disbale add functionality
+    def has_add_permission(self, request):
+        return False
+
+        # This will help you to disable delete functionaliyt
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
     #     # This will help you to disable change functionality
     #
     # def has_change_permission(self, request, obj=None):
@@ -37,3 +38,4 @@ class OrderProductAdmin(admin.ModelAdmin):
 admin.site.register(OrderPlaced, OrderProductAdmin)
 
 
+# admin.site.register(Payment)
