4768324b7d7331b525e725fc9e83452685065b7d
amilmshaji <msamilmanadiyil2560@gmail.com>
2023-04-27 20:30:17 +0530

    question answering

--- chatbot/views.py
+++ chatbot/views.py
@@ -23,7 +23,7 @@ def chatbot(request):
     print("working")
     if request.method == 'POST':
         question = request.POST['question']
-        context = "Django is a powerful and widely used web framework written in Python that allows developers to build robust web applications quickly and efficiently. It follows the Model-View-Template (MVT) architecture, which promotes clean separation of concerns and encourages reusable code. Django provides a plethora of built-in features and tools that simplify common web development tasks such as URL routing, authentication, database management, and form handling."
+        context = "Django is a powerful and widely used web framework written in Python that allows developers to build robust web applications quickly and efficiently. It follows the Model-View-Template (MVT) architecture, which promotes clean separation of concerns and encourages reusable code. Django provides a plethora a of built-in features and tools that simplify common web development tasks such as URL routing, authentication, database management, and form handling."
         answer = answer_question(question, context)
         print("getting")
         print(answer)
